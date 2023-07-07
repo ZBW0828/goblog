fc3b0067b65ad62b1b0b999d852aac9a15188b25
ZBW0828 <2486163701@qq.com>
2023-07-07 15:05:30 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"go-blog/common"
+	"go-blog/router"
+	"log"
+	"net/http"
+)
+
+func init() {
+	//模板加载
+	common.LoadTemplate()
+}
+
+func main() {
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+	//路由
+	router.Router()
+	if err := server.ListenAndServe(); err != nil {
+		log.Println(err)
+	}
+}
